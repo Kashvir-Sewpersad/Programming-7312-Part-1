622392e22babd023c29a772981edf9c07f499089
kashc <kashc@THE_BEAST>
2025-09-07 17:35:14 +0200

    updated reported issues page

--- Services/IssueStorage.cs
+++ Services/IssueStorage.cs
@@ -1,10 +1,13 @@
-﻿using Programming_7312_Part_1.Models;
+﻿
+/*------------------ start of file ------------*/ 
+using Programming_7312_Part_1.Models;
 using System.Collections.Generic;
 
 namespace Programming_7312_Part_1.Services
 {
     public class IssueStorage
     {
-        public LinkedList<Issue> ReportedIssues { get; } = new LinkedList<Issue>();
+        public LinkedList<Issue> ReportedIssues { get; } = new LinkedList<Issue>(); // linked list for the reported issues
     }
 }
+//*------ end of file --------------//
