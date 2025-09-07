aef9f3249a6a790aa6ab6c44200f88f5ac1a004d
kashc <kashc@THE_BEAST>
2025-09-07 19:54:00 +0200

    commented main

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+
+//************************************ start of program ***********************//
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Programming_7312_Part_1.Services;
@@ -6,6 +9,7 @@ namespace Programming_7312_Part_1
 {
     public class Program
     {
+        // start of main method and entry to the program 
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -46,4 +50,6 @@ namespace Programming_7312_Part_1
                     });
                 });
     }
+    //********************** end of main method 
 }
+//***************************** end of program **********************************//
